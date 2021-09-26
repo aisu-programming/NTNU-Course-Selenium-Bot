ce5cde231a9dd0d0c511dcfc969d18d917650c7b
aisu-programming <aisu.programming@gmail.com>
2021-09-26 14:18:46 +0800

    Finished

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
+weights_file_path = "model/val_acc.h5"
+
+
+# ================================================== #
+
+
 """ Libraries """
 import os
 import time
+import datetime
+from selenium.common.exceptions import WebDriverException
 from seleniumwire import webdriver
 import io
-import base64
-from PIL import Image
 import numpy as np
 import random
 
-from model import load_MyModel, id_to_word
-
-
-""" Parameters """
-# None
+from model import id_to_word, resize_image, load_MyModel
 
 
 """ Dictionary """
@@ -36,29 +38,44 @@ def read_account():
         print("Please edit it before run this program again.\n")
 
 
+class BrowserStuckError(Exception):
+    pass
+
+
+class CourseTakenException(Exception):
+    pass
+
+
 def click_and_wait(element):
-    element.click()
-    time.sleep(1)
+    for _ in range(25):
+        try:
+            element.click()
+            time.sleep(1)
+            return
+        except:
+            time.sleep(0.2)
+    raise BrowserStuckError
 
 
 def wait_for_url(driver, url_content):
-    while True:
+    for _ in range(20):
         time.sleep(0.25)
-        if url_content in driver.current_url: break
-    return
+        if url_content in driver.current_url: return
+    raise BrowserStuckError
 
 
 def wait_and_find_element_by_id(driver, id):
-    while True:
+    for _ in range(25):
         try:
             element = driver.find_element_by_id(id)
             return element
         except:
             time.sleep(0.2)
-
+    raise BrowserStuckError
+    
 
 def wait_appeared_element_by_id(driver):
-    while True:
+    for _ in range(20):
         try:
             driver.find_element_by_id("button-1017-btnEl")
             return True
@@ -69,41 +86,44 @@ def wait_appeared_element_by_id(driver):
             return False
         except:
             time.sleep(0.5)
+    raise BrowserStuckError
 
 
 def wait_element_text_by_id(driver, id, texts):
-    while True:
+    for _ in range(25):
         try:
             element = driver.find_element_by_id(id)
             for i, text in enumerate(texts):
-                if text in element.text: return i
+                if text in element.text: return i  # return condition id
+            raise CourseTakenException
         except:
             time.sleep(0.2)
+    raise BrowserStuckError
 
 
 def wait_for_validate_code_img(driver):
-    while True:
+    for _ in range(25):
         if len(driver.find_elements_by_class_name("x-component-default")) == 10:
-            break
+            return
         time.sleep(0.2)
-    return
+    raise BrowserStuckError
 
 
 def wait_for_validate_code_button(driver, button):
-    while True:
+    for _ in range(25):
         buttons = driver.find_elements_by_class_name("x-btn-button")
         if len(buttons) == 19:
-            break
+            if button == "confirm": return buttons[17]
+            else                  : return buttons[18]
         time.sleep(0.2)
-    if button == "confirm": return buttons[17]
-    else                  : return buttons[18]
+    raise BrowserStuckError
 
 
 def get_validate_code_img(driver):
     for request in reversed(driver.requests):
         if "RandImage" in request.url:
             if request.response == None: return None
-            else: return Image.open(io.BytesIO(request.response.body)).convert('L')
+            else: return resize_image(io.BytesIO(request.response.body))
 
 
 def my_predict(model, image):
@@ -115,7 +135,7 @@ def my_predict(model, image):
     return validate_code
 
 
-def calculate_validate_code(validate_code):
+def process_validate_code(validate_code):
     if '=' in validate_code:
         number_1 = number_map[validate_code[0]]
         number_2 = number_map[validate_code[2]]
@@ -123,7 +143,7 @@ def calculate_validate_code(validate_code):
         elif validate_code[1] == '-': return number_1 - number_2
         elif validate_code[1] == '*': return number_1 * number_2
     else:
-        return validate_code
+        return ''.join(validate_code)
 
 
 def login(driver, username, password, model):
@@ -132,7 +152,7 @@ def login(driver, username, password, model):
     wait_and_find_element_by_id(driver, "password-inputEl").send_keys(password)
     validate_code_img = get_validate_code_img(driver)
     validate_code = my_predict(model, validate_code_img)
-    validate_code = calculate_validate_code(validate_code)
+    validate_code = process_validate_code(validate_code)
     wait_and_find_element_by_id(driver, "validateCode-inputEl").send_keys(validate_code)
     click_and_wait(wait_and_find_element_by_id(driver, "button-1016-btnEl"))  # 「登入」按鈕
 
@@ -145,7 +165,7 @@ def login(driver, username, password, model):
             wait_and_find_element_by_id(driver, "password-inputEl").send_keys(password)
             validate_code_img = get_validate_code_img(driver)
             validate_code = my_predict(model, validate_code_img)
-            validate_code = calculate_validate_code(validate_code)
+            validate_code = process_validate_code(validate_code)
             wait_and_find_element_by_id(driver, "validateCode-inputEl").send_keys(validate_code)
             click_and_wait(wait_and_find_element_by_id(driver, "button-1016-btnEl"))  # 「登入」按鈕
 
@@ -157,9 +177,12 @@ def login(driver, username, password, model):
     return
 
 
-def remain_time(now_time, start_time):
-    interval = now_time - start_time
-    return f"{int(interval//60):>2}min {int(interval%60):>2}sec"
+def my_time_str(start_time=None):
+    if start_time is not None:
+        interval = time.time() - start_time
+        return f"{datetime.datetime.now().strftime(f'%H:%M:%S')} - {int(interval//60):>2}min {int(interval%60):>2}sec"
+    else:
+        return f"{datetime.datetime.now().strftime(f'%H:%M:%S')}"
 
 
 def course_taking(driver, course_ids, model):
@@ -167,70 +190,85 @@ def course_taking(driver, course_ids, model):
     start_time = time.time()
 
     while True:
-        for course_id in course_ids:
-            driver.find_element_by_id("serialNo-inputEl").clear()
-            driver.find_element_by_id("serialNo-inputEl").send_keys(course_id)
-            time.sleep(0.2)
 
-            # 驗證碼破圖
-            while True:
-                click_and_wait(driver.find_element_by_id("button-1060-btnEl"))  # 「開課序號直接加選儲存」按鈕
-                wait_for_validate_code_img(driver)
-                validate_code_img = get_validate_code_img(driver)
-                if validate_code_img is not None:
-                    break
-                else:
-                    click_and_wait(wait_for_validate_code_button(driver, "cancel"))  # 「取消」按鈕
-                    print(f"{remain_time(time.time(), start_time)} - Course {course_id}: Validate code image broken. Try again in 3 second.")
-                    time.sleep(3)
+        print(f"{my_time_str(start_time)} - The course ids list now:", course_ids, '\n')
 
-            validate_code = my_predict(model, validate_code_img)
-            validate_code = calculate_validate_code(validate_code)
-            wait_and_find_element_by_id(driver, "valid-inputEl").send_keys(validate_code)
-            click_and_wait(wait_for_validate_code_button(driver, "confirm"))  # 「確認」按鈕
+        try:
+            for course_id in course_ids:
+                driver.find_element_by_id("serialNo-inputEl").clear()
+                driver.find_element_by_id("serialNo-inputEl").send_keys(course_id)
+                time.sleep(0.2)
 
-            # 驗證碼: 正確 或 錯誤
-            while True:
-                condition = wait_element_text_by_id(driver, "messagebox-1001-displayfield-inputEl", ["驗證碼錯誤", "額滿", "儲存成功"])
-                if condition == 0:
-                    print(f"{remain_time(time.time(), start_time)} - Course {course_id}: Validate code {validate_code} incorrect. Try again in 3 second.")
-                    click_and_wait(wait_and_find_element_by_id(driver, "button-1005-btnIconEl"))  # 「OK」按鈕
-                    time.sleep(3)
+                # 驗證碼破圖
+                while True:
+                    click_and_wait(driver.find_element_by_id("button-1060-btnEl"))  # 「開課序號直接加選儲存」按鈕
+                    wait_for_validate_code_img(driver)
+                    validate_code_img = get_validate_code_img(driver)
+                    if validate_code_img is not None:
+                        break
+                    else:
+                        click_and_wait(wait_for_validate_code_button(driver, "cancel"))  # 「取消」按鈕
+                        click_and_wait(wait_and_find_element_by_id(driver, "button-1005-btnIconEl"))  # 「OK」按鈕
+                        print(f"{my_time_str(start_time)} - Course {course_id}: Validate code image broken. Retry in 3 seconds.")
+                        time.sleep(3)
 
-                    # 驗證碼破圖
-                    while True:
-                        click_and_wait(driver.find_element_by_id("button-1060-btnEl"))  # 「開課序號直接加選儲存」按鈕
-                        wait_for_validate_code_img(driver)
-                        validate_code_img = get_validate_code_img(driver)
-                        if validate_code_img is not None:
-                            break
-                        else:
-                            click_and_wait(wait_for_validate_code_button(driver, "cancel"))  # 「取消」按鈕
-                            print(f"{remain_time(time.time(), start_time)} - Course {course_id}: Validate code image broken. Try again in 3 second.")
-                            time.sleep(3)
-                    
-                    validate_code = my_predict(model, validate_code_img)
-                    validate_code = calculate_validate_code(validate_code)
-                    wait_and_find_element_by_id(driver, "valid-inputEl").send_keys(validate_code)
-                    click_and_wait(wait_for_validate_code_button(driver, "confirm"))  # 「確認」按鈕
+                validate_code = my_predict(model, validate_code_img)
+                validate_code = process_validate_code(validate_code)
+                wait_and_find_element_by_id(driver, "valid-inputEl").send_keys(validate_code)
+                click_and_wait(wait_for_validate_code_button(driver, "confirm"))  # 「確認」按鈕
 
-                elif condition == 1:
-                    print(f"{remain_time(time.time(), start_time)} - Course {course_id}: Validate code {validate_code} correct. Full.")
-                    break
+                # 驗證碼: 正確 或 錯誤
+                while True:
+                    condition = wait_element_text_by_id(driver, "messagebox-1001-displayfield-inputEl", ["驗證碼錯誤", "額滿", "儲存成功"])
+                    if condition == 0:
+                        print(f"{my_time_str(start_time)} - Course {course_id}: Validate code '{validate_code}' incorrect. Retry in 3 seconds.")
+                        click_and_wait(wait_and_find_element_by_id(driver, "button-1005-btnIconEl"))  # 「OK」按鈕
+                        time.sleep(3)
 
-                elif condition == 2:
-                    print(f"{remain_time(time.time(), start_time)} - Course {course_id}: Validate code {validate_code} correct. Success!")
-                    course_ids.remove(course_id)
-                    break
-            
-            click_and_wait(wait_and_find_element_by_id(driver, "button-1005-btnIconEl"))  # 「OK」按鈕
-            random_second = 5 + 5 * random.random()
-            print(f"{remain_time(time.time(), start_time)} - Sleep for {random_second} seconds.\n")
-            time.sleep(random_second)
-            if time.time() - start_time > 1200: break
+                        # 驗證碼破圖
+                        validate_code_img_broken_time = 0
+                        while True:
+                            click_and_wait(driver.find_element_by_id("button-1060-btnEl"))  # 「開課序號直接加選儲存」按鈕
+                            wait_for_validate_code_img(driver)
+                            validate_code_img = get_validate_code_img(driver)
+                            if validate_code_img is not None:
+                                break
+                            else:
+                                click_and_wait(wait_for_validate_code_button(driver, "cancel"))  # 「取消」按鈕
+                                click_and_wait(wait_and_find_element_by_id(driver, "button-1005-btnIconEl"))  # 「OK」按鈕
+                                retry_time = validate_code_img_broken_time * 2 + 3
+                                print(f"{my_time_str(start_time)} - Course {course_id}: Validate code image broken. Retry in {retry_time} seconds.")
+                                time.sleep(retry_time)
+                                validate_code_img_broken_time += 1
+                        
+                        validate_code = my_predict(model, validate_code_img)
+                        validate_code = process_validate_code(validate_code)
+                        wait_and_find_element_by_id(driver, "valid-inputEl").send_keys(validate_code)
+                        click_and_wait(wait_for_validate_code_button(driver, "confirm"))  # 「確認」按鈕
 
-        if len(course_ids) == 0: break
-        if time.time() - start_time > 1200: break
+                    elif condition == 1:
+                        print(f"{my_time_str(start_time)} - Course {course_id}: Validate code '{validate_code}' correct. Full.")
+                        break
+
+                    elif condition == 2:
+                        print(f"{my_time_str(start_time)} - Course {course_id}: Validate code '{validate_code}' correct. Success!")
+                        course_ids.remove(course_id)
+                        break
+                
+                click_and_wait(wait_and_find_element_by_id(driver, "button-1005-btnIconEl"))  # 「OK」按鈕
+                random_second = 5 + 5 * random.random()
+                print(f"{my_time_str(start_time)} - Sleep for {random_second:.2} seconds.\n")
+                time.sleep(random_second)
+                if time.time() - start_time > 1170: break  # 19min 30sec
+
+            if len(course_ids) == 0: break
+            if time.time() - start_time > 1170: break  # 19min 30sec
+        
+        except BrowserStuckError:
+            break
+
+        except WebDriverException:
+            break
 
     return course_ids
 
@@ -245,7 +283,7 @@ def main():
         return
 
     # Load predict model
-    model = load_MyModel("model/val_acc.h5")
+    model = load_MyModel(weights_file_path)
     model.summary()
 
     # Read username, password and course ids
@@ -262,6 +300,8 @@ def main():
         course_ids = course_taking(driver, course_ids, model)
         driver.delete_all_cookies()
         if len(course_ids) == 0: break
+        time.sleep(10)
+        print(f"\n{my_time_str()} - Restart a turn.\n")
 
     print("\n\nSuccess! Finish all course taking tasks!\n\n")
     driver.close()
---
+++ model.py
@@ -0,0 +1,158 @@
+import tensorflow as tf
+from PIL import Image
+
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        tf.config.experimental.set_virtual_device_configuration(
+            gpus[0], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024)]
+        )
+    except RuntimeError as e:
+        print(e)
+
+
+class_num = 41
+id_to_word = {
+    0 : 'b',
+    1 : '7',
+    2 : 'e',
+    3 : '*',
+    4 : 'g',
+    5 : '0',
+    6 : 'i',
+    7 : 'c',
+    8 : 'k',
+    9 : '9',
+    10: '+',
+    11: 'z',
+    12: 'l',
+    13: 'r',
+    14: 'w',
+    15: '=',
+    16: '1',
+    17: 'n',
+    18: 'o',
+    19: '3',
+    20: 't',
+    21: 'x',
+    22: 'p',
+    23: '5',
+    24: '8',
+    25: 'v',
+    26: 'h',
+    27: '-',
+    28: 's',
+    29: 'd',
+    30: 'm',
+    31: '4',
+    32: 'j',
+    33: 'u',
+    34: 'q',
+    35: 'f',
+    36: 'a',
+    37: '/',
+    38: 'y',
+    39: '6',
+    40: '2',
+}
+resize_height, resize_width = 60, 216
+
+
+class MyConv(tf.keras.layers.Layer):
+    def __init__(self, filter, kernel_size, strides):
+        super(MyConv, self).__init__()
+        self.cv  = tf.keras.layers.Conv2D(filter, kernel_size=kernel_size, strides=strides, padding="same", use_bias=False)
+        self.bn  = tf.keras.layers.BatchNormalization()
+        self.act = tf.nn.silu
+
+    def call(self, inputs):
+        return self.act(self.bn(self.cv(inputs)))
+
+
+class MyBottleneck(tf.keras.layers.Layer):
+    def __init__(self, filter, shortcut=True):
+        super().__init__()
+        self.cv  = MyConv(filter, kernel_size=3, strides=1)
+        self.add = shortcut
+
+    def forward(self, x):
+        return x + self.cv(x) if self.add else self.cv(x)
+
+
+class MyCSPBottleneck(tf.keras.layers.Layer):
+    def __init__(self, filter, n=1, shortcut=True):
+        super().__init__()
+        self.cv1 = MyConv(filter, kernel_size=1, strides=1)
+        self.b   = [ MyBottleneck(filter, shortcut) for _ in range(n) ]
+        self.cv3 = tf.keras.layers.Conv2D(filter, kernel_size=1, strides=1, use_bias=False)
+        self.bn  = tf.keras.layers.BatchNormalization()
+        self.act = tf.nn.leaky_relu
+        self.cv4 = MyConv(filter, kernel_size=1, strides=1)
+
+    def forward(self, x):
+        y1 = self.cv1(x)
+        for b in self.b: y1 = b(y1)
+        y2 = self.cv3(x)
+        return self.cv4(self.act(self.bn(tf.concat([y1, y2], axis=1)), alpha=0.1))
+
+
+class Detector(tf.keras.layers.Layer):
+    def __init__(self):
+        super(Detector, self).__init__()
+        self.denses = [ tf.keras.Sequential([
+            tf.keras.layers.Dense(64, activation="relu"),
+            tf.keras.layers.Dense(32, activation="relu"),
+            tf.keras.layers.Dense(16, activation="relu"),
+        ]) for _ in range(4) ]
+        self.detect = tf.keras.layers.Dense(class_num, activation="softmax")
+
+    def call(self, x):
+        y = tf.concat([
+            tf.expand_dims(self.detect(self.denses[i](x)), axis=1) for i in range(4)
+        ], axis=1)
+        return y
+
+
+class MyModel(tf.keras.Model):
+    def __init__(self, dropout_rate):
+        super(MyModel, self).__init__()
+        self.cv    = MyConv(32, kernel_size=3, strides=1)
+        self.cv_p1 = MyConv(64, kernel_size=3, strides=2)    # (30, 108,   64)
+        self.bn_p1 = MyCSPBottleneck(64, 1)
+        self.cv_p2 = MyConv(128, kernel_size=3, strides=2)   # (15,  54,  128)
+        self.bn_p2 = MyCSPBottleneck(128, 3)
+        self.cv_p3 = MyConv(256, kernel_size=3, strides=2)   # ( 8,  27,  256)
+        self.bn_p3 = MyCSPBottleneck(256, 15)
+        self.cv_p4 = MyConv(512, kernel_size=3, strides=2)   # ( 4,  14,  512)
+        self.bn_p4 = MyCSPBottleneck(512, 15)
+        self.cv_p5 = MyConv(1024, kernel_size=3, strides=2)  # ( 2,   7, 1024)
+        self.bn_p5 = MyCSPBottleneck(1024, 7)
+        self.cv_p6 = MyConv(2048, kernel_size=3, strides=2)  # ( 1,   4, 2048)
+        self.bn_p6 = MyCSPBottleneck(2048, 7)
+        self.flatten  = tf.keras.layers.Flatten()
+        self.dropout  = tf.keras.layers.Dropout(dropout_rate)
+        self.detector = Detector()
+
+    def call(self, x):
+        x = self.bn_p1(self.cv_p1(self.cv(x)))
+        x = self.bn_p2(self.cv_p2(x))
+        x = self.bn_p3(self.cv_p3(x))
+        x = self.bn_p4(self.cv_p4(x))
+        x = self.bn_p5(self.cv_p5(x))
+        x = self.bn_p6(self.cv_p6(x))
+        y = self.flatten(self.dropout(x))
+        y = self.detector(y)
+        return y
+
+
+def resize_image(img):
+    return Image.open(img).convert('L').resize((resize_width, resize_height))
+
+
+def load_MyModel(h5_filepath):
+    dropout_rate = 0.955
+    model = MyModel(dropout_rate)
+    model.build(input_shape=(None, resize_height, resize_width, 1))
+    model.load_weights(h5_filepath)
+    return model
